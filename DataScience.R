81c6de88abeae2f2305dffbb795920f1d0f5d35f
Gurkarm Singh <gurkarmjot@gmail.com>
2025-01-25 02:01:18 -0700

    First commit

--- DataScience.R
+++ DataScience.R
@@ -9,25 +9,3 @@ list_census_datasets()
 region <- "Alberta"  # Province
 riding <- "Edmonton Gateway"  # Your riding name
 level <- "CSD"  # Level of geography (Census Subdivision)
-
-# Retrieve Census data
-# Replace "CA16" with the appropriate Census year (e.g., 2016)
-data <- get_census(
-  dataset = "CA16",  # 2016 Census
-  regions = list(CSD = riding),  # Query the riding
-  vectors = c("v_CA16_408", "v_CA16_409"),  # Specify census variables
-  geo_format = "sf",  # Retrieve as spatial data
-  level = level
-)
-
-# Display the first few rows of the data
-head(data)
-# Search for variables related to your query
-search_census_vectors("income")
-# Load visualization libraries
-library(ggplot2)
-library(tmap)
-
-# Example: Plot the riding boundary
-tm_shape(data) +
-  tm_polygons("v_CA16_408", title = "Population")  # Replace with a variable of interest
